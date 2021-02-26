502da988c7d31ece033b176f8198bcf69124c71f
adel <sharpshifu@gmail.com>
2021-02-26 10:00:14 +0300

    Form working

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ window.addEventListener('DOMContentLoaded', () => {
         item.classList.toggle('hide');
     };
     const   logo = document.querySelector('.logo'),
-            menu = document.querySelector('.menu__list');
+            menu = document.querySelector('.menu__list'),
+            descr = document.querySelector('.content__descr'),
+            btn = document.querySelector('.login__btn'),
+            username = document.querySelector('#username'),
+            password = document.querySelector('#password');
+    let     users = JSON.parse(localStorage.getItem('users'));
 
-    toggleMenu(menu);
-    logo.addEventListener('click', () => toggleMenu(menu));
+    if (menu != null && logo != null) {
+        toggleMenu(menu);
+        logo.addEventListener('click', () => toggleMenu(menu));
+        if (users != null) {
+            users.forEach((item) => {
+                descr.innerHTML += `
+                    <span>${item}</span>
+                `;
+            });
+        }
+    }
+    if (btn != null) {
+        btn.addEventListener('click', () => {
+            if (users == null) {
+                users = [];
+            }
+            users.push(username.value);
+            localStorage.setItem('users', JSON.stringify(users));
+        });
+    }
 });
